3b8fab0d01973dc3c98afe71f5e8ef2be33aa406
Sparsh Shrestha <97462583+sparshshrestha@users.noreply.github.com>
2024-07-16 20:33:02 -0400

    Add files via upload

--- Tubemetrics.py
+++ Tubemetrics.py
@@ -27,9 +27,32 @@ st.markdown(
     """
     ## Page Descriptions:
 
-    ### 1. Top Youtuber Insight
-    This page provides a comprehensive analysis of top YouTubers through four interactive tabs: 
-    **Subscribers**, **Video Views**, **Number of Videos**, and **Subscribers vs. Video Count vs. Video Views**. 
-    Each tab offers unique insights into YouTube channel performance, visualizing key metrics such as subscriber counts, total video views, video upload volume, and their interrelationships.
+    ### 1. Categorical Analysis:
+    
+    Explore the distribution of top YouTube channels across different content categories. 
+    Use interactive filters to select top YouTubers and categories, then dive into insightful visualizations 
+    such as bar charts, treemaps, and bubble charts. Uncover trends and relationships among YouTube's leading creators 
+    and gain valuable insights into what drives their success in various content categories.
+
+    ### 2. Yearly Analysis:
+    
+    Analyze the growth and performance of top YouTube channels over different years. 
+    Visualize trends in subscriber growth, video views, and video counts across various time periods. 
+    Utilize interactive features to explore individual channel trajectories and understand the evolving landscape 
+    of YouTube content creation over time.
+
+    ### 3. YouTube Views per Video Analysis:
+    
+    Investigate the efficiency and engagement levels of top YouTube channels based on views per video. 
+    Compare and contrast channels across different categories to identify outliers and best practices 
+    in content production and audience engagement strategies. Utilize interactive bar charts and filters 
+    to delve deeper into individual channel metrics and category-wide trends.
+
+    ### 4. Comparative Analysis:
+    
+    Conduct side-by-side comparisons of key metrics between top YouTube influencers. 
+    Select specific influencers and metrics of interest to visualize performance variations 
+    and competitive advantages within the YouTube ecosystem. Leverage interactive features 
+    to uncover insights into successful content strategies and audience engagement tactics.
     """
 )
